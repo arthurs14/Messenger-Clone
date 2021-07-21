ca73710e167e28290ce7d13c4326f2ae96d4b6e1
arthurs14 <arthurcs426@gmail.com>
2021-07-21 14:51:30 -0700

    added navbar and component for seeing a chat ui

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { ChatEngine } from 'react-chat-engine';
+import { auth, chatEngine } from '../firebase';
 
 const Chats = () => {
+  const history = useHistory();
+
+  const handleLogout = async () => {
+    await auth.signOut();
+    history.push('/');
+  };
+
   return (
-    <h1>Chats Component</h1>
+    <div className="chats-page">
+      <div className="nav-bar">
+        <div className="logo-tab">
+          Messenger Clone
+        </div>
+        <div onClick={handleLogout} className="logout-tab">
+          Logout
+        </div>
+      </div>
+
+      <ChatEngine 
+        height="calc(100vh - 66px)"
+        projectId={chatEngine}
+        userName="."
+        userSecret="."
+      />
+    </div>
   );
 };
 
